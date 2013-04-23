cf97aa05c279dcc883ad1c80469f0a07490b5f78
Radu Busuioc <ra.busuioc@gmail.com>
2013-04-23 18:43:59 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DietMeals::Application.config.secret_token = '19fa3596ee3f8e040a595d3276d85004113f0783ec4b7ffb628a4e60acc77ba76769cc5d70f98b178b04e95dd028725f69235ff52574c1dfd851f7ebaf6a3ea7'
