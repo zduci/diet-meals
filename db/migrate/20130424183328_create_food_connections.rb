244a678b065a44ed69c5a5440c857c9541d85611
Radu Busuioc <ra.busuioc@gmail.com>
2013-04-24 19:41:54 +0100

    Rename parent and child connection fields on FoodConnection

--- db/migrate/20130424183328_create_food_connections.rb
+++ db/migrate/20130424183328_create_food_connections.rb
@@ -1,8 +1,8 @@
 class CreateFoodConnections < ActiveRecord::Migration
   def change
     create_table :food_connections do |t|
-      t.integer :parent_food, :null => false
-      t.integer :child_food, :null => false
+      t.integer :parent_food_id, :null => false
+      t.integer :child_food_id, :null => false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130424182401) do
+ActiveRecord::Schema.define(:version => 20130424183328) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(:version => 20130424182401) do
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
   add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
+
+  create_table "food_connections", :force => true do |t|
+    t.integer  "parent_food_id", :null => false
+    t.integer  "child_food_id",  :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
   create_table "foods", :force => true do |t|
     t.string   "name",       :null => false
