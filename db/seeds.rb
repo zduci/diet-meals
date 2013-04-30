999c4f4410a8d88d20ca4278a32324411ee6852d
Radu Busuioc <ra.busuioc@gmail.com>
2013-04-30 18:59:54 +0100

    Basic food connections

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,25 @@
                                                           unit_of_measurement[1])
 end
 
-sugar = Food.create_by_name(:sugar)
-fruit = Food.create_by_name(:fruit)
-tomato = Food.create_by_name(:tomato)
-apple = Food.create_by_name(:apple)
-citrus = Food.create_by_name(:citrus)
-lemon = Food.create_by_name(:lemon)
-vitamins = Food.create_by_name(:vitamin)
+sugar = Food.find_or_create_by_name(:sugar)
+fruit = Food.find_or_create_by_name(:fruit)
+tomato = Food.find_or_create_by_name(:tomato)
+apple = Food.find_or_create_by_name(:apple)
+citrus = Food.find_or_create_by_name(:citrus)
+lemon = Food.find_or_create_by_name(:lemon)
+vitamins = Food.find_or_create_by_name(:vitamin)
+orange = Food.find_or_create_by_name(:orange)
+
+def create_connection(parent, child)
+  FoodConnection.create(:parent_food => parent, :child_food => child)
+end
+
+create_connection(vitamins, fruit)
+create_connection(fruit, tomato)
+create_connection(fruit, apple)
+create_connection(fruit, citrus)
+create_connection(citrus, lemon)
+create_connection(citrus, orange)
+create_connection(sugar, orange)
+create_connection(fruit, apple)
+create_connection(sugar, apple)
