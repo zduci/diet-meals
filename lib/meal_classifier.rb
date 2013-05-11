4d1794af9ecc1e14d90476c3e821b4c28a8f4178
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-11 16:17:41 +0100

    Refactor MealClassifier

--- lib/meal_classifier.rb
+++ lib/meal_classifier.rb
@@ -4,14 +4,24 @@ class MealClassifier
   end
    
   def self.compatible?(diet, meal)
-    unless diet.restrictive
-      (diet.forbidden_foods & meal.ingredients).empty?
+    if diet.restrictive
+      compatible_for_restrictive(diet, meal)
     else
-      if diet.allowed_foods.empty?
-        false
-      else
-        (diet.allowed_foods & meal.ingredients).sort == meal.ingredients
-      end
+      compatible_for_unrestrictive(diet, meal)
     end
+  end
+
+  private
+
+  def self.compatible_for_restrictive(diet, meal)
+    if diet.allowed_foods.empty?
+      false
+    else
+      (diet.allowed_foods & meal.ingredients).sort == meal.ingredients
+    end
+  end
+
+  def self.compatible_for_unrestrictive(diet, meal)
+    (diet.forbidden_foods & meal.ingredients).empty?
   end
 end
