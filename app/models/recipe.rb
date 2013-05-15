9ff80965e58ab5fcc5162f22cb725cb307b15180
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-15 21:59:10 +0100

    Presence validations for Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@ class Recipe < ActiveRecord::Base
   attr_accessible :duration, :instructions, :ingredients, :name
   has_many :ingredients, :include => [:food, :unit_of_measurement], :dependent => :destroy
   has_many :foods, :through => :ingredients
+
+  validates :instructions, :presence => true
+  validates :name, :presence => true
 end
