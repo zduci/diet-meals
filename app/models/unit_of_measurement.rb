cc29ac9040c5bfdda825fd97fea70dce69e172f9
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-15 21:55:52 +0100

    Presence constraints on UnitOfMeasurement

--- app/models/unit_of_measurement.rb
+++ app/models/unit_of_measurement.rb
@@ -2,4 +2,7 @@ class UnitOfMeasurement < ActiveRecord::Base
   attr_accessible :name, :short_name
 
   has_many :ingredients, :dependent => :destroy
+
+  validates :name, :presence => true
+  validates :short_name, :presence => true
 end
