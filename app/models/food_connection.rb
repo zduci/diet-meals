ca6248a5a29b98a0b82fda58c85ce2cbfc9f1372
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-15 21:48:18 +0100

    Uniqueness validation on FoodConnection

--- app/models/food_connection.rb
+++ app/models/food_connection.rb
@@ -3,6 +3,6 @@ class FoodConnection < ActiveRecord::Base
   belongs_to :parent_food, :class_name => 'Food'
   belongs_to :child_food, :class_name => 'Food'
 
-  validates :parent_food_id, :presence => true
+  validates :parent_food_id, :presence => true, :uniqueness => { :scope => :child_food_id }
   validates :child_food_id, :presence => true
 end
