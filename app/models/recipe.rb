8701bb8f877d599a1cc6713614c9e9099cc9bea3
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-15 20:50:01 +0100

    Recipe ingredients include food and unit of measurement

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :duration, :instructions, :ingredients, :name
-  has_many :ingredients
+  has_many :ingredients, :include => [:food, :unit_of_measurement]
   has_many :foods, :through => :ingredients
 end
