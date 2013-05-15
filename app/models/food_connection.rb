e03f015cef679b2f17b07a4c5308bc2cd9dec2c0
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-15 21:47:12 +0100

    Presence validations on FoodConnection

--- app/models/food_connection.rb
+++ app/models/food_connection.rb
@@ -2,4 +2,7 @@ class FoodConnection < ActiveRecord::Base
   attr_accessible :parent_food, :child_food
   belongs_to :parent_food, :class_name => 'Food'
   belongs_to :child_food, :class_name => 'Food'
+
+  validates :parent_food_id, :presence => true
+  validates :child_food_id, :presence => true
 end
