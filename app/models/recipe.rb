91f4837e63200670ecf15a6a7cc8855d82f60cce
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-15 21:03:40 +0100

    Ingredient depends on diet

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :duration, :instructions, :ingredients, :name
-  has_many :ingredients, :include => [:food, :unit_of_measurement]
+  has_many :ingredients, :include => [:food, :unit_of_measurement], :dependent => :destroy
   has_many :foods, :through => :ingredients
 end
