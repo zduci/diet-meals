4a8275c89d6afabf7e0b235d5a04d6105a4b2854
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-16 22:23:28 +0100

    Case insensitive uniqueness validation for food names

--- app/models/food.rb
+++ app/models/food.rb
@@ -15,5 +15,5 @@ class Food < ActiveRecord::Base
 
   has_many :ingredients, :dependent => :destroy
 
-  validates :name, :presence => true, :uniqueness => true
+  validates :name, :presence => true, :uniqueness => { :case_sensitive => false }
 end
