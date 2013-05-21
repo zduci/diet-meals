69f819bf9808dcb4862a1939becf6427c0ef1d46
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-21 22:04:00 +0100

    Refactor FoodGraphConnection

--- lib/food_graph_connection.rb
+++ lib/food_graph_connection.rb
@@ -1,8 +1,7 @@
 class FoodGraphConnection
   def self.create(parent, child)
-    unless FoodConnection.find_by_parent_food_id_and_child_food_id(parent, child)
-      FoodConnection.create(:parent_food => parent,
-                            :child_food => child)
-    end
+    return if FoodConnection.find_by_parent_food_id_and_child_food_id(parent, child)
+    FoodConnection.create(:parent_food => parent,
+                          :child_food => child)
   end
 end
