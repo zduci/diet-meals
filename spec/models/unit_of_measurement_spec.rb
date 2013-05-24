126ffd56cc5ce54dabb5d22e3272710a260129c0
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-24 20:26:53 +0100

    Test uniqueness validations on UnitOfMeasurement

--- spec/models/unit_of_measurement_spec.rb
+++ spec/models/unit_of_measurement_spec.rb
@@ -12,4 +12,14 @@ describe UnitOfMeasurement do
   it 'is invalid without a short name' do
     FactoryGirl.build(:unit_of_measurement, :name => 'meter').should  be_invalid
   end
+
+  it 'is invalid without a unique name' do
+    FactoryGirl.create(:unit_of_measurement, :name => 'meter', :short_name => 'm')
+    FactoryGirl.build(:unit_of_measurement, :name => 'meter', :short_name => 'unique').should be_invalid
+  end
+
+  it 'is invalid without a unique short name' do
+    FactoryGirl.create(:unit_of_measurement, :name => 'meter', :short_name => 'm')
+    FactoryGirl.build(:unit_of_measurement, :name => 'unique', :short_name => 'm').should be_invalid
+  end
 end
