044524a42c7a5a2e18221665a977ef8d6d44fd12
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-24 20:11:43 +0100

    Test Food for name uniqueness

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -8,4 +8,9 @@ describe Food do
   it 'is invalid without a name' do
     FactoryGirl.build(:food).should be_invalid
   end
+
+  it 'is invalid if the name is not unique' do
+    FactoryGirl.create(:food, :name => 'avocado')
+    FactoryGirl.build(:food, :name => 'avocado').should be_invalid
+  end
 end
