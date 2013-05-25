4fc965bca7ed1a9a1436588dc0356c926f1977db
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-25 19:47:58 +0100

    Add uniqueness validation on Diet#exclusive

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -18,4 +18,5 @@ class Diet < ActiveRecord::Base
            :dependent => :destroy
 
   validates :name, :presence => true, :uniqueness => { :case_sensitive => false }
+  validates :exclusive, :presence => true
 end
--- spec/models/diet_spec.rb
+++ spec/models/diet_spec.rb
@@ -2,10 +2,14 @@ require 'spec_helper'
 
 describe Diet do
   it 'can create a valid instance' do
-    FactoryGirl.build(:diet, :name => 'Slow Carb Diet').should be_valid
+    FactoryGirl.build(:diet, :name => 'Slow Carb Diet', :exclusive => true).should be_valid
   end
 
   it 'is invalid without a name' do
-    FactoryGirl.build(:diet).should be_invalid
+    FactoryGirl.build(:diet, :exclusive => true).should be_invalid
+  end
+
+  it 'is invalid without an exclusive attribute' do
+    FactoryGirl.build(:diet, :name => 'Slow Carb Diet').should be_invalid
   end
 end
