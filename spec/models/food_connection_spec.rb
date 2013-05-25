3523423eb5ba58a4fc817a2d99aafce87a260b33
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-25 19:13:35 +0100

    Test presence validations on FoodConnection, refactor

--- spec/models/food_connection_spec.rb
+++ spec/models/food_connection_spec.rb
@@ -4,16 +4,33 @@ describe FoodConnection do
   let(:fruit) { FactoryGirl.create(:food, :name => :fruit) }
   let(:orange) { FactoryGirl.create(:food, :name => :orange) }
 
-  it 'creates a connection between foods if it does not exist' do
-    FoodConnection.connect(fruit, orange)
+  context 'Food#connect' do
+    it 'creates a connection between foods if it does not exist' do
+      FoodConnection.connect(fruit, orange)
 
-    orange.parent_foods.should == [fruit]
-    fruit.child_foods.should == [orange]
+      orange.parent_foods.should == [fruit]
+      fruit.child_foods.should == [orange]
+    end
+
+    it 'does not create a connection between foods if it already exists' do
+      FoodConnection.connect(fruit, orange)
+      FoodConnection.should_not_receive(:create)
+      FoodConnection.connect(fruit, orange)
+    end
   end
 
-  it 'does not create a connection between foods if it already exists' do
-    FoodConnection.connect(fruit, orange)
-    FoodConnection.should_not_receive(:create)
-    FoodConnection.connect(fruit, orange)
+  context 'validations' do
+    it 'can create a valid object' do
+      FactoryGirl.build(:food_connection, :parent_food => fruit, :child_food => orange).should be_valid
+    end
+
+    it 'is invalid without a parent food' do
+      FactoryGirl.build(:food_connection, :child_food => orange).should be_invalid
+    end
+
+    it 'is invalid without a child food' do
+      FactoryGirl.build(:food_connection, :parent_food => fruit,).should be_invalid
+    end
   end
+
 end
