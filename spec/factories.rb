c5d22af242cddca694ae1a0781258383c2e4d31e
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-25 19:58:12 +0100

    Add factories for boiled egg

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,24 @@
 FactoryGirl.define do
   factory :food do
+    name 'egg'
   end
 
   factory :food_connection do
   end
 
   factory :unit_of_measurement do
+    name 'gram'
+    short_name 'g'
   end
 
   factory :diet do
+  end
+
+  factory :ingredient do
+  end
+
+  factory :recipe do
+    name 'Boiled egg'
+    instructions 'Boil the egg.'
   end
 end
