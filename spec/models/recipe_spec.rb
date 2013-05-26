2798d4c333eda6062d25b7924a8a7108eddd2f70
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-26 22:03:48 +0100

    Group specs

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,33 +1,37 @@
 require 'spec_helper'
 
 describe Recipe do
-  it 'can create a valid instance with no duration' do
-    FactoryGirl.build(:recipe, :instructions => 'Boil the egg', :name => 'Boiled egg').should be_valid
+  context 'validations' do
+    it 'can create a valid instance with no duration' do
+      FactoryGirl.build(:recipe, :instructions => 'Boil the egg', :name => 'Boiled egg').should be_valid
+    end
+
+    it 'can create a valid instance with duration' do
+      FactoryGirl.build(:recipe, :instructions => 'Boil the egg', :name => 'Boiled egg', :duration => 30).should be_valid
+    end
+
+    it 'is invalid without instructions' do
+      FactoryGirl.build(:recipe, :name => 'Boiled egg').should be_invalid
+    end
+
+    it 'is invalid without a name' do
+      FactoryGirl.build(:recipe, :instructions => 'Boil the egg').should be_invalid
+    end
+
+    it 'is invalid if the duration is less than 1' do
+      FactoryGirl.build(:recipe, :instructions => 'Boil the egg', :name => 'Boiled egg', :duration => 0).should be_invalid
+    end
   end
 
-  it 'can create a valid instance with duration' do
-    FactoryGirl.build(:recipe, :instructions => 'Boil the egg', :name => 'Boiled egg', :duration => 30).should be_valid
-  end
+  context 'alternative constructors' do
+    it 'creates a new recipe by name and instructions' do
+      Recipe.should_receive(:create!).with(:name => 'Boiled egg', :instructions => 'boil the egg')
+      Recipe.create_recipe('Boiled egg', 'boil the egg')
+    end
 
-  it 'is invalid without instructions' do
-    FactoryGirl.build(:recipe, :name => 'Boiled egg').should be_invalid
-  end
-
-  it 'is invalid without a name' do
-    FactoryGirl.build(:recipe, :instructions => 'Boil the egg').should be_invalid
-  end
-
-  it 'is invalid if the duration is less than 1' do
-    FactoryGirl.build(:recipe, :instructions => 'Boil the egg', :name => 'Boiled egg', :duration => 0).should be_invalid
-  end
-
-  it 'creates a new recipe by name and instructions' do
-    Recipe.should_receive(:create!).with(:name => 'Boiled egg', :instructions => 'boil the egg')
-    Recipe.create_recipe('Boiled egg', 'boil the egg')
-  end
-
-  it 'creates a new recipe by name, instructions and duration' do
-    Recipe.should_receive(:create!).with(:name => 'Boiled egg', :instructions => 'boil the egg', :duration => 30)
-    Recipe.create_recipe('Boiled egg', 'boil the egg', 30)
+    it 'creates a new recipe by name, instructions and duration' do
+      Recipe.should_receive(:create!).with(:name => 'Boiled egg', :instructions => 'boil the egg', :duration => 30)
+      Recipe.create_recipe('Boiled egg', 'boil the egg', 30)
+    end
   end
 end
