2cd8152de4d17561429e6ded6a754f2b02289220
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-26 14:03:22 +0100

    Refactor

--- spec/models/diet_spec.rb
+++ spec/models/diet_spec.rb
@@ -1,40 +1,46 @@
 require 'spec_helper'
 
 describe Diet do
-  it 'can create a valid instance' do
-    FactoryGirl.build(:diet, :name => 'Slow Carb Diet', :exclusive => true).should be_valid
+  context 'validations' do
+    it 'can create a valid instance' do
+      FactoryGirl.build(:diet, :name => 'Slow Carb Diet', :exclusive => true).should be_valid
+    end
+
+    it 'is invalid without a name' do
+      FactoryGirl.build(:diet, :exclusive => true).should be_invalid
+    end
+
+    it 'is invalid without an exclusive attribute' do
+      FactoryGirl.build(:diet, :name => 'Slow Carb Diet').should be_invalid
+    end
+
+    it 'is invalid without a unique name' do
+      FactoryGirl.create(:diet, :name => 'Slow Carb Diet', :exclusive => true)
+      FactoryGirl.build(:diet, :name => 'Slow Carb Diet', :exclusive => true).should be_invalid
+    end
+
+    it 'is valid for inclusive diets' do
+      FactoryGirl.build(:diet, :name => 'Inclusive diet', :exclusive => true).should be_valid
+    end
   end
 
-  it 'is invalid without a name' do
-    FactoryGirl.build(:diet, :exclusive => true).should be_invalid
+  context 'types of diet' do
+    it 'checks if it is exclusive' do
+      FactoryGirl.build(:diet, :name => 'Exclusive diet', :exclusive => true).should be_exclusive
+    end
+
+    it 'checks if it is inclusive' do
+      FactoryGirl.build(:diet, :name => 'Inclusive diet', :exclusive => false).should be_inclusive
+    end
   end
 
-  it 'is invalid without an exclusive attribute' do
-    FactoryGirl.build(:diet, :name => 'Slow Carb Diet').should be_invalid
-  end
+  context 'constructors' do
+    it 'creates inclusive diets' do
+      Diet.create_inclusive('inclusive diet').should be_inclusive
+    end
 
-  it 'is invalid without a unique name' do
-    FactoryGirl.create(:diet, :name => 'Slow Carb Diet', :exclusive => true)
-    FactoryGirl.build(:diet, :name => 'Slow Carb Diet', :exclusive => true).should be_invalid
-  end
-
-  it 'is valid for inclusive diets' do
-    FactoryGirl.build(:diet, :name => 'Inclusive diet', :exclusive => true).should be_valid
-  end
-
-  it 'checks if it is exclusive' do
-    FactoryGirl.build(:diet, :name => 'Exclusive diet', :exclusive => true).should be_exclusive
-  end
-
-  it 'checks if it is inclusive' do
-    FactoryGirl.build(:diet, :name => 'Inclusive diet', :exclusive => false).should be_inclusive
-  end
-
-  it 'creates inclusive diets' do
-    Diet.create_inclusive('inclusive diet').should be_inclusive
-  end
-
-  it 'creates exclusive diets' do
-    Diet.create_exclusive('exclusive diet').should be_exclusive
+    it 'creates exclusive diets' do
+      Diet.create_exclusive('exclusive diet').should be_exclusive
+    end
   end
 end
