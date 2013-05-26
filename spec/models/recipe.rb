47a9f3dcf4f6d067e8ccee8e6722079070c29668
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-26 11:13:59 +0100

    Use correct factory

--- spec/models/recipe.rb
+++ spec/models/recipe.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Recipe do
   it 'can create a valid instance' do
-    FactoryGirl.build(:instructions => 'Boil the egg', :name => 'Boiled egg').should be_valid
+    FactoryGirl.build(:recipe, :instructions => 'Boil the egg', :name => 'Boiled egg').should be_valid
   end
 
   it 'can create a valid instance' do
-    FactoryGirl.build(:name => 'Boiled egg').should be_invalid
+    FactoryGirl.build(:recipe, :name => 'Boiled egg').should be_invalid
   end
 
   it 'is invalid without a name' do
-    FactoryGirl.build(instructions => 'Boil the egg').should be_invalid
+    FactoryGirl.build(:recipe, :instructions => 'Boil the egg').should be_invalid
   end
 end
