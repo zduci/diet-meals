10e12a1987173a6ba2fc2dc6c56afcb97fa3ee21
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-26 21:05:57 +0100

    Clearer spec names

--- spec/lib/meal_classifier_inclusive_diets_spec.rb
+++ spec/lib/meal_classifier_inclusive_diets_spec.rb
@@ -17,25 +17,25 @@ describe MealClassifier do
     MealClassifier.compatible?(empty_inclusive_diet, empty_meal).should be_true
   end
 
-  it 'rejects meals containing forbidden ingredients' do
+  it 'rejects meals containing forbidden foods' do
     no_rice = stub(:exclusive? => false, :forbidden_foods => [rice], :allowed_foods => [])
     MealClassifier.compatible?(no_rice, rice_meal).should be_false
   end
 
-  it 'rejects meals containing child foods of forbidden ingredients' do
+  it 'rejects meals containing child foods of forbidden foods' do
     MealClassifier.compatible?(no_carbs, rice_meal).should be_false
   end
 
-  it 'rejects meals containing descendant foods of forbidden ingredients' do
+  it 'rejects meals containing descendant foods of forbidden foods' do
     MealClassifier.compatible?(no_carbs, brown_rice_meal).should be_false
   end
 
-  it 'accepts meals containing allowed ingredients with forbidden parent foods' do
+  it 'accepts meals containing allowed foods with forbidden parent foods' do
     no_carbs_except_rice = stub(:exclusive? => false, :forbidden_foods => [carbs], :allowed_foods => [rice])
     MealClassifier.compatible?(no_carbs_except_rice, rice_meal).should be_true
   end
 
-  it 'accepts meals containing allowed ingredients with forbidden ancestor foods' do
+  it 'accepts meals containing allowed foods with forbidden ancestor foods' do
     no_carbs_except_brown_rice = stub(:exclusive? => false, :forbidden_foods => [carbs], :allowed_foods => [brown_rice])
     MealClassifier.compatible?(no_carbs_except_brown_rice, brown_rice_meal).should be_true
   end
