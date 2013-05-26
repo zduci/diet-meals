d9fdc693f7cad6caebb972cf044acdfe6a92aeb9
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-26 10:53:24 +0100

    Add assertions to Ingredient spec

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -10,18 +10,18 @@ describe Ingredient do
   end
 
   it 'is invalid without a food' do
-    FactoryGirl.build(:ingredient, :unit_of_measurement => unit_of_measurement, :recipe => recipe, :quantity => 1)
+    FactoryGirl.build(:ingredient, :unit_of_measurement => unit_of_measurement, :recipe => recipe, :quantity => 1).should be_invalid
   end
 
   it 'is invalid without a recipe' do
-    FactoryGirl.build(:ingredient, :food => food, :unit_of_measurement => unit_of_measurement, :quantity => 1)
+    FactoryGirl.build(:ingredient, :food => food, :unit_of_measurement => unit_of_measurement, :quantity => 1).should be_invalid
   end
 
   it 'is invalid without a unit of measurement' do
-    FactoryGirl.build(:ingredient, :food => food, :recipe => recipe, :quantity => 1)
+    FactoryGirl.build(:ingredient, :food => food, :recipe => recipe, :quantity => 1).should be_invalid
   end
 
   it 'is invalid without a quantity' do
-    FactoryGirl.build(:ingredient, :food => food, :unit_of_measurement => unit_of_measurement, :recipe => recipe)
+    FactoryGirl.build(:ingredient, :food => food, :unit_of_measurement => unit_of_measurement, :recipe => recipe).should be_invalid
   end
 end
