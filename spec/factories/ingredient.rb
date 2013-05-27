c6cb77675b955faa6bb95afc8eef20e73f49012a
Radu Busuioc <ra.busuioc@gmail.com>
2013-05-27 13:03:08 +0100

    Refactor Ingredient spec

--- spec/factories/ingredient.rb
+++ spec/factories/ingredient.rb
@@ -1,4 +1,8 @@
 FactoryGirl.define do
   factory :ingredient do
+    food_id 1
+    unit_of_measurement_id 1
+    recipe_id 1
+    quantity 1
   end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,38 +1,37 @@
 require 'spec_helper'
 
 describe Ingredient do
-  let(:food) { FactoryGirl.create(:egg) }
-  let(:unit_of_measurement) { FactoryGirl.create(:gram) }
-  let(:recipe) { FactoryGirl.create(:boiled_egg) }
-
   context 'validations' do
     it 'can create a valid object' do
-      FactoryGirl.build(:ingredient, :food => food, :unit_of_measurement => unit_of_measurement, :recipe => recipe, :quantity => 1).should be_valid
+      FactoryGirl.build(:ingredient).should be_valid
     end
 
     it 'is invalid without a food' do
-      FactoryGirl.build(:ingredient, :unit_of_measurement => unit_of_measurement, :recipe => recipe, :quantity => 1).should be_invalid
+      FactoryGirl.build(:ingredient, :food => nil).should be_invalid
     end
 
     it 'is invalid without a recipe' do
-      FactoryGirl.build(:ingredient, :food => food, :unit_of_measurement => unit_of_measurement, :quantity => 1).should be_invalid
+      FactoryGirl.build(:ingredient, :recipe => nil).should be_invalid
     end
 
     it 'is invalid without a unit of measurement' do
-      FactoryGirl.build(:ingredient, :food => food, :recipe => recipe, :quantity => 1).should be_invalid
+      FactoryGirl.build(:ingredient, :unit_of_measurement => nil).should be_invalid
     end
 
     it 'is invalid without a quantity' do
-      FactoryGirl.build(:ingredient, :food => food, :unit_of_measurement => unit_of_measurement, :recipe => recipe).should be_invalid
+      FactoryGirl.build(:ingredient, :quantity => nil).should be_invalid
     end
 
     it 'is invalid without a positive value greater than 0' do
-      FactoryGirl.build(:ingredient, :food => food, :unit_of_measurement => unit_of_measurement, :recipe => recipe, :quantity => 0).should be_invalid
+      FactoryGirl.build(:ingredient, :quantity => 0).should be_invalid
     end
   end
 
   context 'alternative constructors' do
     it 'creates ingredients by recipe, food, unit_of_measurement and quantity' do
+      recipe = stub(:recipe)
+      food = stub(:food)
+      unit_of_measurement = stub(:unit_of_measurement)
       Ingredient.should_receive(:create!).with(:recipe => recipe, :food => food, :unit_of_measurement => unit_of_measurement, :quantity  => 1)
       Ingredient.create_ingredient(recipe, food, unit_of_measurement, 1)
     end
