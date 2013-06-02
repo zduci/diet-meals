8cfdd86de5aef96bedae61c13af1c65b0f42b82f
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-02 17:55:10 +0100

    AddMeal creates new meal

--- lib/add_meal.rb
+++ lib/add_meal.rb
@@ -1,4 +1,5 @@
 class AddMeal
   def self.add(name, instructions, duration, *ingredients)
+    Meal.create_meal(name, instructions, duration)
   end
 end
--- spec/lib/add_meal_spec.rb
+++ spec/lib/add_meal_spec.rb
@@ -1,8 +1,13 @@
+require 'spec_helper'
 require_relative '../../lib/add_meal'
 
 describe AddMeal do
   describe '#add' do
-    it 'creates a new meal'
+    it 'creates a new meal' do
+      Meal.should_receive(:create_meal).with('boiled eggs', 'boil the eggs, add salt', 5)
+      AddMeal.add('boiled eggs', 'boil the eggs, add salt', 5)
+    end
+
     it 'creates the ingredients for the new meal'
     it 'classifies the meal into diets'
   end
