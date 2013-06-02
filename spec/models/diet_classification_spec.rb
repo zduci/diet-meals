36544790f3ade3184b9764318949e5e110bad786
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-02 20:48:08 +0100

    Refactor spec

--- spec/models/diet_classification_spec.rb
+++ spec/models/diet_classification_spec.rb
@@ -1,22 +1,26 @@
 require 'spec_helper'
 
 describe DietClassification do
-  it 'can create a valid instance' do
-    FactoryGirl.build(:diet_classification).should be_valid
+  describe 'validations' do
+    it 'can create a valid instance' do
+      FactoryGirl.build(:diet_classification).should be_valid
+    end
+
+    it 'is not valid without a meal' do
+      FactoryGirl.build(:diet_classification, :meal_id => nil).should be_invalid
+    end
+
+    it 'is not valid without a diet' do
+      FactoryGirl.build(:diet_classification, :diet_id => nil).should be_invalid
+    end
   end
 
-  it 'is not valid without a meal' do
-    FactoryGirl.build(:diet_classification, :meal_id => nil).should be_invalid
-  end
-
-  it 'is not valid without a diet' do
-    FactoryGirl.build(:diet_classification, :diet_id => nil).should be_invalid
-  end
-
-  it 'creates instances with meal and diet' do
-    meal = stub(:meal)
-    diet = stub(:diet)
-    DietClassification.should_receive(:create!).with(:meal => meal, :diet => diet)
-    DietClassification.create_classification(meal, diet)
+  describe 'DietClassification#create_classifiction' do
+    it 'creates instances with meal and diet' do
+      meal = stub(:meal)
+      diet = stub(:diet)
+      DietClassification.should_receive(:create!).with(:meal => meal, :diet => diet)
+      DietClassification.create_classification(meal, diet)
+    end
   end
 end
