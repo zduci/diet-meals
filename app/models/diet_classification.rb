c6a919875107dd565a5fbca789cb193bc23b1af5
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-02 20:34:54 +0100

    Add presence validation for meal_id DietClassification

--- app/models/diet_classification.rb
+++ app/models/diet_classification.rb
@@ -1,3 +1,5 @@
 class DietClassification < ActiveRecord::Base
   attr_accessible :diet, :meal
+
+  validates :meal_id, :presence => true
 end
--- spec/models/diet_classification_spec.rb
+++ spec/models/diet_classification_spec.rb
@@ -4,4 +4,8 @@ describe DietClassification do
   it 'can create a valid instance' do
     FactoryGirl.build(:diet_classification).should be_valid
   end
+
+  it 'is not valid without a valid meal' do
+    FactoryGirl.build(:diet_classification, :meal_id => nil).should be_invalid
+  end
 end
