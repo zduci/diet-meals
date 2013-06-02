eb40a1c8208d349a9fe1a8dc321c07c42f8c1784
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-02 16:05:52 +0100

    Add meal spec

---
+++ spec/lib/add_meal_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../lib/add_meal'
+
+describe AddMeal do
+  describe '#add' do
+    it 'creates a new meal'
+    it 'creates the ingredients for the new meal'
+    it 'classifies the meal into diets'
+  end
+end
