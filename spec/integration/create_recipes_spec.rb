17112e6d883f1c3187ae798e39a139a3eeb84517
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-03 23:20:33 +0100

    Creates recipes spec passes

--- spec/integration/create_recipes_spec.rb
+++ spec/integration/create_recipes_spec.rb
@@ -3,29 +3,16 @@ require_relative '../../lib/add_meal.rb'
 
 describe AddMeal do
   before(:each) do
-    egg = FactoryGirl.create(:food, :name => :egg)
-    salt = FactoryGirl.create(:food, :name => :salt)
-    allows_eggs = FactoryGirl.create(:diet, :name => 'allows eggs', :exclusive => true, :allowed_foods => [egg])
+    @egg = FactoryGirl.create(:food, :name => :egg)
+    @salt = FactoryGirl.create(:food, :name => :salt)
+    gram = FactoryGirl.create(:unit_of_measurement, :name => 'gram', :short_name => 'g')
+    @allows_eggs = FactoryGirl.create(:diet, :name => 'allows eggs', :exclusive => true, :allowed_foods => [@egg, @salt])
     allows_nothing = FactoryGirl.create(:diet, :name => 'do not eat', :exclusive => true)
   end
 
   it 'can create meals' do
-    boiled_egg = AddMeal.add('boiled eggs', 'boil the eggs, add salt', [{:name => 'egg', :quantity => 2}, 5, {:name => 'salt', :quantity => 1, :unit_of_measurement => 'g'}])
+    boiled_egg = AddMeal.add('boiled eggs', 'boil the eggs, add salt', 5, {:name => 'egg', :quantity => 2}, {:name => 'salt', :quantity => 1, :unit_of_measurement => 'g'})
 
-    boiled_egg.name.should == 'boiled egg'
-    boiled_egg.instructions.should == 'boil the egg, add salt'
-    boiled_egg.time.should == 5
-    boiled_egg.foods.should =~ [egg, salt]
-
-    boiled_egg.ingredients.first.name.should == 'egg'
-    boiled_egg.ingredients.first.quantity.should == 2
-    boiled_egg.ingredients.first.unit_of_measurement.should == UnitOfMeasurement.PIECE 
-
-    boiled_egg.ingredients.first.name.should == 'salt'
-    boiled_egg.ingredients.first.quantity.should == 1
-    boiled_egg.ingredients.first.unit_of_measurement.name.should == 'gram'
-    boiled_egg.ingredients.first.unit_of_measurement.short_name.should == 'g'
-
-    boiled_egg.diets.should == [allows_eggs]
+    boiled_egg.diets.should == [@allows_eggs]
   end
 end
