ee8164c3210e40b40f43745e1ed33b95b2d491fa
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-09 09:53:54 +0100

    Associations specs for Food

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,16 +1,26 @@
 require 'spec_helper'
 
 describe Food do
-  it 'can create a valid object' do
-    FactoryGirl.build(:food).should be_valid
+  context 'validations' do
+    it 'can create a valid object' do
+      FactoryGirl.build(:food).should be_valid
+    end
+
+    it { should validate_presence_of(:name) }
+
+    it { should validate_uniqueness_of(:name) }
+
+    it 'creates an instance by name' do
+      Food.should_receive(:create!).with(:name => 'avocado')
+      Food.create_food('avocado')
+    end
   end
 
-  it { should validate_presence_of(:name) }
+  context 'associations' do
+    it { should have_many(:parent_foods) }
 
-  it { should validate_uniqueness_of(:name) }
+    it { should have_many(:child_foods) }
 
-  it 'creates an instance by name' do
-    Food.should_receive(:create!).with(:name => 'avocado')
-    Food.create_food('avocado')
+    it { should have_many(:ingredients) }
   end
 end
