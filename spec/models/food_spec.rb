25a88760837d36179c6859c45e441ceddf1041ff
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-09 09:39:52 +0100

    Refactor food spec

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,15 +1,13 @@
 require 'spec_helper'
 
 describe Food do
-  let(:food) { FactoryGirl.build(:food) }
-
   it 'can create a valid object' do
-    food.should be_valid
+    FactoryGirl.build(:food).should be_valid
   end
 
-  it { food.should validate_presence_of(:name) }
+  it { should validate_presence_of(:name) }
 
-  it { food.should validate_uniqueness_of(:name) }
+  it { should validate_uniqueness_of(:name) }
 
   it 'creates an instance by name' do
     Food.should_receive(:create!).with(:name => 'avocado')
