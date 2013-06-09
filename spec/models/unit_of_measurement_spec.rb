89dc0c759255a2d59795f31a3ca7aac10bcca1e8
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-09 09:54:31 +0100

    Associations specs for UnitOfMeasurement

--- spec/models/unit_of_measurement_spec.rb
+++ spec/models/unit_of_measurement_spec.rb
@@ -1,26 +1,32 @@
 require 'spec_helper'
 
 describe UnitOfMeasurement do
-  it 'can create a valid object' do
-    FactoryGirl.build(:unit_of_measurement).should  be_valid
+  context 'validations' do
+    it 'can create a valid object' do
+      FactoryGirl.build(:unit_of_measurement).should  be_valid
+    end
+
+    it { should validate_presence_of(:name) }
+
+    it { should validate_presence_of(:short_name) }
+
+    it 'is invalid without a unique name' do
+      FactoryGirl.create(:unit_of_measurement, :name => 'meter', :short_name => 'm')
+      should validate_uniqueness_of(:name)
+    end
+
+    it 'is invalid without a unique short name' do
+      FactoryGirl.create(:unit_of_measurement, :name => 'meter', :short_name => 'm')
+      should validate_uniqueness_of(:short_name)
+    end
+
+    it 'creates a new instance by name and short name' do
+      UnitOfMeasurement.should_receive(:create!).with(:name => 'meter', :short_name => 'm')
+      UnitOfMeasurement.create_unit_of_measurement('meter', 'm')
+    end
   end
 
-  it { should validate_presence_of(:name) }
-
-  it { should validate_presence_of(:short_name) }
-
-  it 'is invalid without a unique name' do
-    FactoryGirl.create(:unit_of_measurement, :name => 'meter', :short_name => 'm')
-    should validate_uniqueness_of(:name)
-  end
-
-  it 'is invalid without a unique short name' do
-    FactoryGirl.create(:unit_of_measurement, :name => 'meter', :short_name => 'm')
-    should validate_uniqueness_of(:short_name)
-  end
-
-  it 'creates a new instance by name and short name' do
-    UnitOfMeasurement.should_receive(:create!).with(:name => 'meter', :short_name => 'm')
-    UnitOfMeasurement.create_unit_of_measurement('meter', 'm')
+  context 'associations' do
+    it { should have_many(:ingredients) }
   end
 end
