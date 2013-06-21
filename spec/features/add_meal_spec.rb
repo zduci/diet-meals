a5a0f0c78a6c225f6b2d823ee8c0b22a4dbb7fa5
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-21 19:43:30 +0100

    Use new tag ids in add meal feature

--- spec/features/add_meal_spec.rb
+++ spec/features/add_meal_spec.rb
@@ -4,9 +4,9 @@ describe 'adding a meal' do
   before(:each) do
     @egg = FactoryGirl.create(:food, :name => :egg)
     @salt = FactoryGirl.create(:food, :name => :salt)
-    gram = FactoryGirl.create(:unit_of_measurement, :name => 'gram', :short_name => 'g')
+    FactoryGirl.create(:unit_of_measurement, :name => 'gram', :short_name => 'g')
     @allows_eggs = FactoryGirl.create(:diet, :name => 'allows eggs', :exclusive => true, :allowed_foods => [@egg, @salt])
-    allows_nothing = FactoryGirl.create(:diet, :name => 'do not eat', :exclusive => true)
+    FactoryGirl.create(:diet, :name => 'do not eat eggs', :exclusive => false, :forbidden_foods => [@egg])
   end
 
   it 'can create meals' do
@@ -14,14 +14,17 @@ describe 'adding a meal' do
     fill_in 'meal_name', :with => 'boiled eggs'
     fill_in 'meal_instructions', :with => 'boil the eggs, add salt'
     fill_in 'meal_duration', :with => 5
-    fill_in 'meal_ingredients_0_name', :with => 'egg'
-    fill_in 'meal_ingredients_0_quantity', :with => 2
-    fill_in 'meal_ingredients_1_name', :with => 'salt'
-    fill_in 'meal_ingredients_1_unit_of_measurement', :with => 'g'
-    fill_in 'meal_ingredients_1_quantity', :with => 2
+
+    fill_in 'meal_ingredients_attributes_0_food_name', :with => 'egg'
+    fill_in 'meal_ingredients_attributes_0_quantity', :with => 2
+
+    fill_in 'meal_ingredients_attributes_1_food_name', :with => 'salt'
+    fill_in 'meal_ingredients_attributes_1_unit_of_measurement_short_name', :with => 'g'
+    fill_in 'meal_ingredients_attributes_1_quantity', :with => 2
     
     click_button 'Add Meal'
 
     page.should have_content 'allows eggs'
+    page.should_not have_content 'do not eat eggs'
   end
 end
