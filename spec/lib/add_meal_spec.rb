9721bc4e196a56f4e8ad2e20e6f5ca1a707b2f81
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-22 12:07:26 +0100

    Add meal spec works with new params format

--- spec/lib/add_meal_spec.rb
+++ spec/lib/add_meal_spec.rb
@@ -11,46 +11,59 @@ describe AddMeal do
       let(:diet) { stub(:diet) }
 
       before(:each) do
-        Meal.stub(:create_meal).with('boiled eggs', 'boil the eggs, add salt', 5) { meal }
+        Meal.stub(:create_meal).with('boiled eggs', 'boil the eggs, add salt', '5') { meal }
         Food.stub(:find_by_name!).with('egg') { egg }
         Food.stub(:find_by_name!).with('salt') { salt }
         UnitOfMeasurement.stub(:find_by_short_name!).with('g') { gram }
-        Ingredient.stub(:create_ingredient).with(meal, egg, 2)
-        Ingredient.stub(:create_ingredient).with(meal, salt, 1, gram)
+        Ingredient.stub(:create_ingredient).with(meal, egg, '2')
+        Ingredient.stub(:create_ingredient).with(meal, salt, '1', gram)
         MealClassifier.stub(:classify).with(meal) { [diet] }
         DietClassification.stub(:create_classification).with(meal, diet)
       end
 
+      def add_meal
+        AddMeal.add('boiled eggs', 'boil the eggs, add salt', '5', {:food => {:name => 'egg'}, :quantity => '2', :unit_of_measurement => {:short_name => ''}}, {:food => {:name => 'salt'}, :quantity => '1', :unit_of_measurement =>{ :short_name =>'g'}})
+      end
+
       it 'creates a new meal' do
-        Meal.should_receive(:create_meal).with('boiled eggs', 'boil the eggs, add salt', 5)
-        AddMeal.add('boiled eggs', 'boil the eggs, add salt', 5)
+        Meal.should_receive(:create_meal).with('boiled eggs', 'boil the eggs, add salt', '5')
+        add_meal
       end
 
       it 'creates the ingredients for the new meal' do
-        Ingredient.should_receive(:create_ingredient).with(meal, egg, 2)
-        Ingredient.should_receive(:create_ingredient).with(meal, salt, 1, gram)
-        AddMeal.add('boiled eggs', 'boil the eggs, add salt', 5, {:name => 'egg', :quantity => 2}, {:name => 'salt', :quantity => 1, :unit_of_measurement => 'g'})
+        Ingredient.should_receive(:create_ingredient).with(meal, egg, '2')
+        Ingredient.should_receive(:create_ingredient).with(meal, salt, '1', gram)
+        add_meal
       end
 
       it 'classifies the meal into diets' do
         DietClassification.should_receive(:create_classification).with(meal, diet)
-        AddMeal.add('boiled eggs', 'boil the eggs, add salt', 5, {:name => 'egg', :quantity => 2}, {:name => 'salt', :quantity => 1, :unit_of_measurement => 'g'})
+        add_meal
       end
 
       it 'reloads the meal at the end' do
         meal.should_receive(:reload)
-        AddMeal.add('boiled eggs', 'boil the eggs, add salt', 5, {:name => 'egg', :quantity => 2}, {:name => 'salt', :quantity => 1, :unit_of_measurement => 'g'})
+        add_meal
       end
     end
 
     context 'error handling' do
       it 'fails for inexistent food name' do
-        expect{ AddMeal.add('boiled eggs', 'boil the eggs, add salt', 5, {:name => 'egg', :quantity => 2}) }.to raise_error ActiveRecord::RecordNotFound
+        expect{ 
+          AddMeal.add('boiled eggs', 'boil the eggs, add salt', '5', 
+                      { :food => {:name => 'does not exist'}, 
+                        :quantity => '2'}) 
+        }.to raise_error ActiveRecord::RecordNotFound
       end
 
       it 'fails for inexistent unit of measurement short name' do
         FactoryGirl.create(:food, :name => 'salt')
-        expect{ AddMeal.add('boiled eggs', 'boil the eggs, add salt', 5, {:name => 'salt', :quantity => 1, :unit_of_measurement => 'g'}) }.to raise_error ActiveRecord::RecordNotFound
+        expect{ 
+          AddMeal.add('boiled eggs', 'boil the eggs, add salt', '5', 
+                      { :food => {:name => 'salt'}, 
+                        :quantity => '1', 
+                        :unit_of_measurement => { :short_name => 'does not exist'}}) 
+        }.to raise_error ActiveRecord::RecordNotFound
       end
     end
   end
