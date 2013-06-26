d1e909d92b7a742d8ca90838fca2c7fe57a36530
Radu Busuioc <ra.busuioc@gmail.com>
2013-06-26 08:32:05 +0100

    Simplify the process of adding new meals using AddMeal

--- lib/add_meal.rb
+++ lib/add_meal.rb
@@ -1,32 +1,14 @@
 class AddMeal
   def self.from_params(meal_params)
-    add(meal_params[:name], 
-        meal_params[:instructions], 
-        MealDuration.to_minutes(meal_params[:duration_hours], meal_params[:duration_minutes]),
-        *meal_params[:ingredients_attributes].values)
-  end
-
-  def self.add(name, instructions, duration, *ingredients)
-    meal = Meal.create_meal(name, instructions, duration)
-    create_ingredients_for(meal, ingredients)
-    create_classifications(meal)
-    meal.reload
+    meal = Meal.new(add_duration(meal_params))
+    create_classifications(meal) if  meal.save
+    meal
   end
 
   private
-  def self.create_ingredients_for(meal, ingredients)
-    ingredients.each { |ingredient_attributes| create_ingredient_for(meal, ingredient_attributes) }
-  end
-
-  def self.create_ingredient_for(meal, ingredient)
-    food = Food.find(ingredient.fetch(:food).fetch(:id))
-    quantity = ingredient[:quantity]
-    unit_of_measurement_id = ingredient.fetch(:unit_of_measurement).fetch(:id)
-    unit_of_measurement = UnitOfMeasurement.find(unit_of_measurement_id)
-    Ingredient.create_ingredient(meal, 
-                                 food,
-                                 quantity,
-                                 unit_of_measurement)
+  def self.add_duration(meal_params)
+    meal_params[:duration] = MealDuration.to_minutes(meal_params[:duration_hours], meal_params[:duration_minutes])
+    meal_params.except(:duration_minutes, :duration_hours)
   end
 
   def self.create_classifications(meal)
--- spec/lib/add_meal_spec.rb
+++ spec/lib/add_meal_spec.rb
@@ -2,91 +2,60 @@ require 'spec_helper'
 require_relative '../../lib/add_meal'
 
 describe AddMeal do
-  describe '#add' do
-    context 'adding a new meal' do
-      let(:meal) { stub(:meal, :reload => true)  }
-      let(:egg) { stub(:egg)  }
-      let(:salt) { stub(:salt)  }
-      let(:gram) { stub(:gram) }
-      let(:piece) { stub(:piece) }
-      let(:diet) { stub(:diet) }
-
-      before(:each) do
-        Meal.stub(:create_meal).with('boiled eggs', 'boil the eggs, add salt', '5') { meal }
-        Food.stub(:find).with('10') { egg }
-        Food.stub(:find).with('20') { salt }
-        UnitOfMeasurement.stub(:find).with('30') { gram }
-        UnitOfMeasurement.stub(:find).with('') { piece }
-        Ingredient.stub(:create_ingredient).with(meal, egg, '2', piece)
-        Ingredient.stub(:create_ingredient).with(meal, salt, '1', gram)
-        MealClassifier.stub(:classify).with(meal) { [diet] }
-        DietClassification.stub(:create_classification).with(meal, diet)
-      end
-
-      def add_meal
-        AddMeal.add('boiled eggs', 'boil the eggs, add salt', '5', {:food => {:id => '10'}, :quantity => '2', :unit_of_measurement => {:id => ''}}, {:food => {:id => '20'}, :quantity => '1', :unit_of_measurement =>{ :id =>'30'}})
-      end
-
-      it 'creates a new meal' do
-        Meal.should_receive(:create_meal).with('boiled eggs', 'boil the eggs, add salt', '5')
-        add_meal
-      end
-
-      it 'creates the ingredients for the new meal' do
-        Ingredient.should_receive(:create_ingredient).with(meal, egg, '2', piece)
-        Ingredient.should_receive(:create_ingredient).with(meal, salt, '1', gram)
-        add_meal
-      end
-
-      it 'classifies the meal into diets' do
-        DietClassification.should_receive(:create_classification).with(meal, diet)
-        add_meal
-      end
-
-      it 'reloads the meal at the end' do
-        meal.should_receive(:reload)
-        add_meal
-      end
-    end
-
-    context 'error handling' do
-      it 'fails for inexistent food id' do
-        expect{ 
-          AddMeal.add('boiled eggs', 'boil the eggs, add salt', '5', 
-                      { :food => {:id => 'does not exist'}, 
-                        :quantity => '2'}) 
-        }.to raise_error ActiveRecord::RecordNotFound
-      end
-
-      it 'fails for inexistent unit of measurement short name' do
-        FactoryGirl.create(:food, :id => '1')
-        expect{ 
-          AddMeal.add('boiled eggs', 'boil the eggs, add salt', '5', 
-                      { :food => {:id => '1'}, 
-                        :quantity => '1', 
-                        :unit_of_measurement => { :id => 'does not exist'}}) 
-        }.to raise_error ActiveRecord::RecordNotFound
-      end
-    end
-  end
-
   describe '#from_params' do
-    let(:name) { 'toast' }
-    let(:instructions) { 'toast the bread' }
-    let(:duration) { '3' }
-    let(:duration_hours) { '0' }
-    let(:duration_minutes) { '3' }
-    let(:ingredient) { {'food' => { 'name' => 'bread' }, 'quantity' => '2', 'unit_of_measurement' => { 'id' => '13'}} }
-    let(:meal_params) { {:name => name,
+    context 'adding a new meal' do
+      let(:name) { 'toast' }
+      let(:instructions) { 'toast the bread' }
+      let(:duration) { '3' }
+      let(:duration_hours) { '0' }
+      let(:duration_minutes) { '3' }
+      let(:ingredient) { {'food' => { 'name' => 'bread' }, 'quantity' => '2', 'unit_of_measurement' => { 'id' => '13'}} }
+      let(:meal_params) { {:name => name,
                          :instructions => instructions,
                          :duration_hours => duration_hours,
                          :duration_minutes => duration_minutes,
                          :ingredients_attributes => {'0' => ingredient}} }
+      let(:meal_params_with_duration) { {:name => name,
+                         :instructions => instructions,
+                         :duration => duration,
+                         :ingredients_attributes => {'0' => ingredient}} }
+      before(:each) do
+        MealDuration.stub(:to_minutes).with(duration_hours, duration_minutes) { duration }
+        #MealClassifier.stub(:classify).with(meal) { [diet] }
+        #DietClassification.stub(:create_classification).with(meal, diet)
+      end
 
-    it 'calls #add with the correct parameters' do
-      MealDuration.stub(:to_minutes).with(duration_hours, duration_minutes) { duration }
-      AddMeal.should_receive(:add).with(name, instructions, duration, ingredient)
-      AddMeal.from_params(meal_params)
+      def add_meal
+        AddMeal.from_params(meal_params)
+      end
+
+      it 'creates a new meal' do
+        meal = stub(:meal, :save => true)
+        diet = stub
+        Meal.should_receive(:new).with(meal_params_with_duration) { meal }
+        meal.should_receive(:save)
+        MealClassifier.stub(:classify).with(meal) { [diet] }
+        DietClassification.stub(:create_classification).with(meal, diet)
+        add_meal
+      end
+
+      it 'classifies the meal into diets' do
+        meal = stub(:meal, :save => true)
+        diet = stub
+        Meal.stub(:new).with(meal_params_with_duration) { meal }
+        MealClassifier.stub(:classify).with(meal) { [diet] }
+        DietClassification.should_receive(:create_classification).with(meal, diet)
+        add_meal
+      end
+
+      it 'returns the meal at the end' do
+        meal = stub(:meal, :save => true)
+        diet = stub
+        Meal.stub(:new).with(meal_params_with_duration) { meal }
+        MealClassifier.stub(:classify).with(meal) { [diet] }
+        DietClassification.stub(:create_classification).with(meal, diet)
+        add_meal.should == meal
+      end
     end
   end
 end
