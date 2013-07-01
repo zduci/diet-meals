5ba2393920985b1bc0e095af6eca5563bc4f7d46
Radu Busuioc <ra.busuioc@gmail.com>
2013-07-01 19:59:49 +0100

    Add AddStar

---
+++ lib/add_star.rb
@@ -0,0 +1,9 @@
+class AddStar
+  def self.add(classification_id, user)
+    if Star.create_star(classification_id, user)
+      MealDietClassification.stars_count(classification_id)
+    else 
+      false
+    end
+  end
+end
