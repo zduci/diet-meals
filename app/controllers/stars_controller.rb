bcf6c6332f25f37d1f84c17fcc469a15dfb481e2
Radu Busuioc <ra.busuioc@gmail.com>
2013-07-01 20:51:43 +0100

    StarsController returns empty object if it fails to create new star

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,6 +1,10 @@
 class StarsController < ApplicationController
   def create
     stars_count = AddStar.add(params[:meal_diet_classification_id], current_user)
-    render :json => {:stars_count => stars_count}
+    if stars_count
+      render :json => {:stars_count => stars_count}
+    else
+      render :json => {}
+    end
   end
 end
--- spec/controllers/stars_controller_spec.rb
+++ spec/controllers/stars_controller_spec.rb
@@ -3,27 +3,39 @@ require 'spec_helper'
 describe StarsController do
   describe '#create' do
     context 'user is logged in' do
-      it 'tries to create a new star' do
-        classification_id = '1'
-        user = FactoryGirl.create(:user)
-        sign_in user
+      let(:classification_id) { '1' }
+      let(:user) { FactoryGirl.create(:user) }
 
+      before(:each) do
+        sign_in user
+      end
+
+      def do_post
+        post :create, :meal_diet_classification_id => classification_id
+      end
+
+      it 'tries to create a new star' do
         AddStar.should_receive(:add).with(classification_id, user)
 
-        post :create, :meal_diet_classification_id => classification_id
+        do_post
       end
       
       context 'creates a new star' do
         it 'returns the stars count' do
-          classification_id = '1'
-          user = FactoryGirl.create(:user)
-          sign_in user
           stars_count = 1
-
           AddStar.stub(:add).with(classification_id, user) { stars_count }
 
-          post :create, :meal_diet_classification_id => classification_id
+          do_post
           response.body.should == "{\"stars_count\":#{stars_count}}"
+        end
+      end
+
+      context 'fails to create a new star' do
+        it 'returns the stars count' do
+          AddStar.stub(:add).with(classification_id, user) { false }
+
+          do_post
+          response.body.should == "{}"
         end
       end
     end
