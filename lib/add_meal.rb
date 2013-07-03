db773eca76ca4ad8d86dcc07016c48d7afc5f7d5
Radu Busuioc <ra.busuioc@gmail.com>
2013-07-03 20:33:27 +0100

    AddMeal adds user

--- lib/add_meal.rb
+++ lib/add_meal.rb
@@ -1,11 +1,20 @@
 class AddMeal
-  def self.from_params(meal_params)
-    meal = Meal.new(add_duration(meal_params))
+  def self.from_params(meal_params, user)
+    meal = Meal.new(add_duration_and_user(meal_params, user))
     create_classifications(meal) if  meal.save
     meal
   end
 
   private
+  def self.add_duration_and_user(meal_params, user)
+    add_user(add_duration(meal_params), user)
+  end
+
+  def self.add_user(meal_params, user)
+    meal_params[:user_id] = user.id
+    meal_params
+  end
+
   def self.add_duration(meal_params)
     meal_params[:duration] = MealDuration.to_minutes(meal_params[:duration_hours], meal_params[:duration_minutes])
     meal_params.except(:duration_minutes, :duration_hours)
--- spec/lib/add_meal_spec.rb
+++ spec/lib/add_meal_spec.rb
@@ -9,27 +9,37 @@ describe AddMeal do
     let(:duration_hours) { '0' }
     let(:duration_minutes) { '3' }
     let(:ingredient) { {'food' => { 'name' => 'bread' }, 'quantity' => '2', 'unit_of_measurement' => { 'id' => '13'}} }
-    let(:meal_params) { {:name => name,
+    let(:user_id) { 1 }
+    let(:user) { stub(:user, :id => user_id) }
+    let(:meal_params) { 
+                      {:name => name,
                        :instructions => instructions,
                        :duration_hours => duration_hours,
                        :duration_minutes => duration_minutes,
                        :ingredients_attributes => {'0' => ingredient}} }
-    let(:meal_params_with_duration) { {:name => name,
+    let(:meal_params_with_duration) { 
+                      {:name => name,
                        :instructions => instructions,
                        :duration => duration,
+                       :ingredients_attributes => {'0' => ingredient}} }
+    let(:meal_params_with_duration_and_user) { 
+                      {:name => name,
+                       :instructions => instructions,
+                       :duration => duration,
+                       :user_id => user_id,
                        :ingredients_attributes => {'0' => ingredient}} }
     before(:each) do
       MealDuration.stub(:to_minutes).with(duration_hours, duration_minutes) { duration }
     end
 
     def add_meal
-      AddMeal.from_params(meal_params)
+      AddMeal.from_params(meal_params, user)
     end
 
     it 'creates a new meal' do
       meal = stub(:meal, :save => true)
       diet = stub
-      Meal.should_receive(:new).with(meal_params_with_duration) { meal }
+      Meal.should_receive(:new).with(meal_params_with_duration_and_user) { meal }
       meal.should_receive(:save)
       MealClassifier.stub(:classify).with(meal) { [diet] }
       MealDietClassification.stub(:create_classification).with(meal, diet)
@@ -39,7 +49,7 @@ describe AddMeal do
     it 'classifies the meal into diets' do
       meal = stub(:meal, :save => true)
       diet = stub
-      Meal.stub(:new).with(meal_params_with_duration) { meal }
+      Meal.stub(:new).with(meal_params_with_duration_and_user) { meal }
       MealClassifier.stub(:classify).with(meal) { [diet] }
       MealDietClassification.should_receive(:create_classification).with(meal, diet)
       add_meal
@@ -48,7 +58,7 @@ describe AddMeal do
     it 'returns the meal at the end' do
       meal = stub(:meal, :save => true)
       diet = stub
-      Meal.stub(:new).with(meal_params_with_duration) { meal }
+      Meal.stub(:new).with(meal_params_with_duration_and_user) { meal }
       MealClassifier.stub(:classify).with(meal) { [diet] }
       MealDietClassification.stub(:create_classification).with(meal, diet)
       add_meal.should == meal
