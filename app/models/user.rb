14b1c7853043685d68201ca8036eabbf56c20b06
Radu Busuioc <ra.busuioc@gmail.com>
2013-07-03 19:55:08 +0100

    User has many Meals

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :meals
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,7 @@ describe User do
     it 'can create a valid object' do
       FactoryGirl.build(:user).should be_valid
     end
+
+    it { should have_many(:meals) }
   end
 end
