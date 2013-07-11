729ac197ec943722db4264f75b1c86381f724245
Radu Busuioc <ra.busuioc@gmail.com>
2013-07-11 21:28:00 +0100

    Add link_to_star helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def link_to_stars(meal_diet_classification, current_user)
+    star = meal_diet_classification.starred_by?(current_user)
+    return link_to 'Unstar', star, :remote => true, :method => :delete, :id => "unstar_#{meal_diet_classification.id}"
+  rescue ActiveRecord::RecordNotFound => e
+    return link_to 'Star', stars_path(:meal_diet_classification_id => meal_diet_classification.id), :remote => true, :method => :post, :id => "star_#{meal_diet_classification.id}"
+  end
 end
