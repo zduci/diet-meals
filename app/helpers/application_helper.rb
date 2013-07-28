8afc23390d2e60b20bf9c6a73add5c02350ac563
Radu Busuioc <ra.busuioc@gmail.com>
2013-07-28 14:41:26 +0100

    Dynamically add ingredients to meal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def ingredient_attributes(f)
+    f.fields_for(:ingredients, Ingredient.new, :child_index => "new_ingredient") do |builder|
+      render 'ingredient_fields', :f => builder
+    end
+  end
 end
