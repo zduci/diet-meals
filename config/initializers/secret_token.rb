f3a58b716962da453902963a35740bfe9871c2e7
Radu Busuioc <ra.busuioc@gmail.com>
2013-07-30 19:01:32 +0100

    Add secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 DietMeals::Application.config.secret_token = '19fa3596ee3f8e040a595d3276d85004113f0783ec4b7ffb628a4e60acc77ba76769cc5d70f98b178b04e95dd028725f69235ff52574c1dfd851f7ebaf6a3ea7'
+DietMeals::Application.config.secret_key_base = '8e28e3c42af331b2e4bf1147ed26b71114a66d7df2e0b48d2caf00c02d1025d604e31bc52a9b6864a813f499144cf9d9ee1186876c4dd649098d4ab06c5fb92d'
